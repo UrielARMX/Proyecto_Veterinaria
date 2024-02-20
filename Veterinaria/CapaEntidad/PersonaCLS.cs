b6151828180041e51bb51cc893a3ff7203d1c987
UrielARMX <armenta21uriel@gmail.com>
2024-02-19 22:17:37 -0800

    Creacion de la Capa Entidad
    Se creo la capa entidad y la clase PersonaCLS que mapea los campos de la tabla Persona

---
+++ Veterinaria/CapaEntidad/PersonaCLS.cs
@@ -0,0 +1,10 @@
+﻿namespace CapaEntidad
+{
+    public class PersonaCLS
+    {
+        private int iid { get; set; }
+        private string nombreCompleto { get; set; }
+        private string correo {  get; set; }
+        private string fechaNacimientoCadena { get; set; }
+    }
+}
