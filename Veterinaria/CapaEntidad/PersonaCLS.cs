d930c6843d505c37c1add2832b31040d3d5bf95a
UrielARMX <armenta21uriel@gmail.com>
2024-02-19 23:00:22 -0800

    Modificacion del Controller Persona y Clase PersonaCLS
    Se crearon dos metodos GET mas en el Controller Persona y se agregaron mas campos a la clase PersonaCLS de la capa entidad

--- Veterinaria/CapaEntidad/PersonaCLS.cs
+++ Veterinaria/CapaEntidad/PersonaCLS.cs
@@ -6,5 +6,11 @@
         public string nombreCompleto { get; set; }
         public string correo {  get; set; }
         public string fechaNacimientoCadena { get; set; }
+
+        public string nombre { get; set; }
+        public string apellidoPaterno { get; set; }
+        public string apellidoMaterno { get; set; }
+        public int iidsexo { get; set; }
+        public DateTime fechaNacimiento { get; set; }
     }
 }
--- Veterinaria/WebAPI/Controllers/PersonaController.cs
+++ Veterinaria/WebAPI/Controllers/PersonaController.cs
@@ -32,8 +32,65 @@ namespace WebAPI.Controllers
             }
             catch (Exception)
             {
+                return Lista;
+            }
+        }
 
-                throw;
+        [HttpGet("{nombrePersona}")]
+        public List<PersonaCLS> ListarPersona(string nombrePersona)
+        {
+            List<PersonaCLS> Lista = new List<PersonaCLS>();
+
+            try
+            {
+                using (DbAa529bDbveterinariaContext db = new DbAa529bDbveterinariaContext())
+                {
+                    Lista = (from persona in db.Personas
+                             where persona.Bhabilitado == 1 && (persona.Nombre + persona.Appaterno + persona.Apmaterno).Contains(nombrePersona)
+                             select new PersonaCLS
+                             {
+                                 iid = persona.Iidpersona,
+                                 nombreCompleto = persona.Nombre + " " + persona.Appaterno + " " + persona.Apmaterno,
+                                 correo = persona.Correo,
+                                 fechaNacimientoCadena = persona.Fechanacimiento == null ? "" : persona.Fechanacimiento.Value.ToString("dd/MM/yyyy")
+                             }).ToList();
+                    return Lista;
+                }
+            }
+            catch (Exception)
+            {
+                return Lista;
+            }
+        }
+
+        [HttpGet("MostrarPersonaPorId/{id}")]
+        public PersonaCLS ListarPersonaPorId(int id)
+        {
+            PersonaCLS oPersona = new PersonaCLS();
+
+            try
+            {
+                using (DbAa529bDbveterinariaContext db = new DbAa529bDbveterinariaContext())
+                {
+                    oPersona = (from persona in db.Personas
+                             where persona.Bhabilitado == 1 && persona.Iidpersona == id
+                             select new PersonaCLS
+                             {
+                                 iid = persona.Iidpersona,
+                                 nombre = persona.Nombre, 
+                                 apellidoPaterno = persona.Appaterno,
+                                 apellidoMaterno = persona.Apmaterno,
+                                 correo = persona.Correo,
+                                 iidsexo = (int)persona.Iidsexo,
+                                 fechaNacimiento = (DateTime)persona.Fechanacimiento,
+                                 fechaNacimientoCadena = persona.Fechanacimiento == null ? "" : persona.Fechanacimiento.Value.ToString("dd/MM/yyyy")
+                             }).First();
+                    return oPersona;
+                }
+            }
+            catch (Exception)
+            {
+                return oPersona;
             }
         }
     }
