364ded969dc6b63599f6ba593585fc05908cdcb7
UrielARMX <armenta21uriel@gmail.com>
2024-02-20 20:17:29 -0800

    Habilitacion del HttpClient
    Se habilito el HttpClient y se inyecto en el controller persona

--- ProyectoNetCore/ProyectoNetCore/Controllers/PersonaController.cs
+++ ProyectoNetCore/ProyectoNetCore/Controllers/PersonaController.cs
@@ -5,10 +5,12 @@ namespace ProyectoNetCore.Controllers
     public class PersonaController : Controller
     {
         private string _baseurl;
+        private readonly IHttpClientFactory _httpClientFactory;
 
-        public PersonaController(IConfiguration configuration)
+        public PersonaController(IConfiguration configuration, IHttpClientFactory httpClientFactory)
         {
             _baseurl = configuration["baseurl"];
+            _httpClientFactory = httpClientFactory;
         }
         public IActionResult Index()
         {
