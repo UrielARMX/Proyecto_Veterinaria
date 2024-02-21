2ea62b4d4b7471187e6bc46f59f243f7afd8457f
UrielARMX <armenta21uriel@gmail.com>
2024-02-20 19:58:54 -0800

    Creacion de la Vista
    Se creo la vista y el controller de Persona

---
+++ ProyectoNetCore/ProyectoNetCore/Controllers/PersonaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoNetCore.Controllers
+{
+    public class PersonaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
