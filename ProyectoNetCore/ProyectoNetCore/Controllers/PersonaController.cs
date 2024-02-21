c8a4015b49604568b7c6ab85e42affe42eead49d
UrielARMX <armenta21uriel@gmail.com>
2024-02-20 20:12:03 -0800

    Guardado de la URL de la API
    Guarde la URL de la API  en una variable

--- ProyectoNetCore/ProyectoNetCore/Controllers/PersonaController.cs
+++ ProyectoNetCore/ProyectoNetCore/Controllers/PersonaController.cs
@@ -4,6 +4,12 @@ namespace ProyectoNetCore.Controllers
 {
     public class PersonaController : Controller
     {
+        private string _baseurl;
+
+        public PersonaController(IConfiguration configuration)
+        {
+            _baseurl = configuration["baseurl"];
+        }
         public IActionResult Index()
         {
             return View();
